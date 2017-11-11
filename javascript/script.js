1538261ded23f7d364b0e7a74ed8fbb9574875da
John Doe <johndoe@example.com>
2017-11-11 16:42:51 +0200

    masive

---
+++ javascript/script.js
@@ -0,0 +1,47 @@
+// var masive = ["hand","lag","mause","abdomen"];
+// var i = 0;
+// var string = " ";
+// while (masive.length > i){
+// 	if (masive[i] != masive[masive.length - 1]){
+// 		string += masive[i] + "-";
+// 	}else {
+// 		string += masive[masive.length - 1];
+// 	}
+// 	i++;
+// }
+// alert(string);
+
+
+
+// var masive = ["google", "bot", "meen", "botle"];
+// var flag = false ;
+
+// while (flag == false){
+// 	var add = prompt("добавьте слово или введите стоп");
+// 	if (add == "стоп"){
+// 		flag = true ;
+// 	}else {
+// 		masive += "," + add;
+// 	}
+	
+// }
+// alert(masive);
+// console.log(masive);
+
+// var string = "javascript";
+// var i = 0;
+//  var letter = prompt("введите букву");
+ 
+//  while (i < string.length) {
+//    if (letter == string[i]) {
+//     	alert(i + 1);
+//      	break;
+//    }
+//    i++;
+//  }
+//  if (string[i] != letter) {
+//    alert('-1');
+//  }
+
+
+ 
